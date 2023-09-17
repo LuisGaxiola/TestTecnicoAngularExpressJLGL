f685dd9fc034bab26a7eb767a5dd6573070ca534
Jose Luis Gaxiola Lopez <joseluisg64@gmail.com>
2023-09-17 04:46:26 +0000

    Crear backend en express.js

---
+++ BackendExpress/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
+});
